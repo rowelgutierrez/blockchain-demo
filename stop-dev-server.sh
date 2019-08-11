960a38c4cf43f7430123f80696946186659ad16a
rowel <rowel@elephantventures.com>
2019-08-11 19:35:55 +0800

    Fixed stop script

--- stop-dev-server.sh
+++ stop-dev-server.sh
@@ -1,14 +1,11 @@
 cd ./hyperledger-fabric/dev-server
 
-if [ ! -f run.pid ]; then
-    echo "Server is already stopped!"
-else
+if [ -f run.pid ]; then
     kill -9 $(cat run.pid)
     rm -f run.pid
-
-    export FABRIC_VERSION=hlfv12
-
-    bash stopFabric.sh
 fi
+
+export FABRIC_VERSION=hlfv12
+bash stopFabric.sh
 
 cd ../../
