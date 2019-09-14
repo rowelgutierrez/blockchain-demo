07f2b7f80402b349d0251b13c244636de2319b69
rowel <rowel@elephantventures.com>
2019-09-14 23:16:46 +0800

    Update start script

--- start.sh
+++ start.sh
@@ -28,5 +28,7 @@ sleep ${FABRIC_START_TIMEOUT}
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.imfreemobile.com/msp" peer0.org1.imfreemobile.com peer channel create -o orderer.imfreemobile.com:7050 -c userschannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.imfreemobile.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.imfreemobile.com/msp" peer0.org1.imfreemobile.com peer channel join -b userschannel.block
+# Update peer anchor peers
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.imfreemobile.com/msp" peer0.org1.imfreemobile.com peer channel update -o orderer.imfreemobile.com:7050 -c userschannel -f /etc/hyperledger/configtx/Org1MSPanchors.tx
 
 cd ../../
