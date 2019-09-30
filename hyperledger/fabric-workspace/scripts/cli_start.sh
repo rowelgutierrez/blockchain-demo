99f95dd86a787b35dc906a97d0b2d04173b0be05
rowel <rowel@elephantventures.com>
2019-09-30 20:40:04 +0800

    checkpoint

--- hyperledger/fabric-workspace/scripts/cli_start.sh
+++ hyperledger/fabric-workspace/scripts/cli_start.sh
@@ -1,14 +1,18 @@
 # Chaincode path
 # CC_SRC_PATH=/opt/gopath/src/github.com/user-management
+WORKDIR=/var/hyperledger/fabric/peer
+
 if [ -z "${CC_SRC_PATH}" ]
 then
-    CC_SRC_PATH=/var/hyperledger/fabric/peer/chaincode
+    CC_SRC_PATH=$WORKDIR/chaincode
 fi
 
 if [ -z "${CHANNEL_ARTIFACTS_DIR}" ]
 then
-    CHANNEL_ARTIFACTS_DIR=/var/hyperledger/fabric/peer/channel-artifacts
+    CHANNEL_ARTIFACTS_DIR=$WORKDIR/channel-artifacts
 fi
+
+echo "Artifacts Directory: $CHANNEL_ARTIFACTS_DIR"
 
 if [ "${DEV_MODE}" == "true" ]
 then
@@ -19,6 +23,10 @@ else
     ORDERER_ADDRESS="orderer-imfreemobile-com"
 fi
 
+echo "Orderer Address: $ORDERER_ADDRESS"
+
+SLEEP_TIMER=10
+
 # Users channel name
 USERS_CHANNEL=userschannel
 
@@ -27,32 +35,47 @@ MSG=$(peer channel getinfo -c userschannel)
 if [[ $string != *"Invalid chain ID, userschannel"* ]]; then
     echo "Creating userschannel..."
 
-    peer channel create -o "$ORDERER_ADDRESS:7050" -c "$USERS_CHANNEL" -f $CHANNEL_ARTIFACTS_DIR/channel.tx
-    peer channel join -b userschannel.block
+    peer channel create -o $ORDERER_ADDRESS:7050 -c "$USERS_CHANNEL" -f $CHANNEL_ARTIFACTS_DIR/channel.tx
+
+    echo "Joining userschannel..."
+    sleep $SLEEP_TIMER
+
+    peer channel join -b $WORKDIR/userschannel.block
+
+    echo "Updating peer anchor..."
+    sleep $SLEEP_TIMER
 
     # Update peer anchor peer for Org1
-    peer channel update -o "$ORDERER_ADDRESS:7050" -c "$USERS_CHANNEL" -f $CHANNEL_ARTIFACTS_DIR/Org1MSPanchors.tx
+    peer channel update -o $ORDERER_ADDRESS:7050 -c "$USERS_CHANNEL" -f $CHANNEL_ARTIFACTS_DIR/Org1MSPanchors.tx
 else
     echo "Channel userschannel already exists!"
 fi
 
 # Install node_modules
+echo "Installing node libraries..."
+
 cd $CC_SRC_PATH/user-management
 npm install
 
 # Register admin user
+echo "Registering users..."
 node enrollAdmin.js
 node registerSampleUser.js
 
-echo "Install chaincode $CC_SRC_PATH"
-peer chaincode install -n userm -v 1.0 -p "$CC_SRC_PATH" -l node
+echo "Install chaincode $CC_SRC_PATH/user-management"
+sleep $SLEEP_TIMER
+
+peer chaincode install -n userm -v 1.0 -p "$CC_SRC_PATH/user-management" -l node
 
 echo "Instantiate chaincode in channel $USERS_CHANNEL"
+sleep $SLEEP_TIMER
+
 peer chaincode instantiate -o "$ORDERER_ADDRESS:7050" -C "$USERS_CHANNEL" -n userm -l node -v 1.0 -c '{"Args":[]}' -P "AND ('Org1MSP.member')"
 # peer chaincode instantiate -o orderer.example.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init","a","100","b","200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')" >&log.txt
-sleep 10
 
 echo "Invoke user-management initLedger func"
+sleep $SLEEP_TIMER
+
 peer chaincode invoke -o "$ORDERER_ADDRESS:7050" -C "$USERS_CHANNEL" -n userm -c '{"function":"initLedger","Args":[]}'
 
 # Test chaincode
