cb539299d665ba26faa979275b126613115aa5b9
rowel <rowel@elephantventures.com>
2019-10-14 15:50:33 +0800

    refactor scripts

--- dev-stop.sh
+++ dev-stop.sh
@@ -1,5 +1,21 @@
-cd ./hyperledger/fabric-network/scripts
+export BASE_DIR=$(pwd)
+
+if [ "$1" = "reset" ]; then
+    echo "Deleting certs and artifacts"
+
+    cd $BASE_DIR/hyperledger/fabric-network
+
+    rm -rf channel-artifacts/channel.tx
+    rm -rf channel-artifacts/genesis.block
+    rm -rf channel-artifacts/Org1MSPanchors.tx
+    rm -rf crypto-config
+    rm -rf lock
+
+    cd ../../
+fi
+
+cd $BASE_DIR/hyperledger/fabric-network/scripts
 
 docker-compose down -v
 
-cd ../../../
+cd $BASE_DIR
