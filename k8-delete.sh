3ba04b76e85c61fc37b0d1d050473e0c224e9c0b
rowel <rowel@elephantventures.com>
2019-10-21 17:02:47 +0800

    k8 for apiserver (nodeport only)

--- k8-delete.sh
+++ k8-delete.sh
@@ -1,5 +1,5 @@
 kubectl delete deployments $(kubectl get deployments --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}') &&
-kubectl delete services ca-imfreemobile-com couchdb orderer-imfreemobile-com peer0-org1-imfreemobile-com &&
+kubectl delete services api-server ca-imfreemobile-com couchdb orderer-imfreemobile-com peer0-org1-imfreemobile-com &&
 
 if [ "$1" = "--include-volumes" ]; then
     kubectl delete pv shared-pv &&
